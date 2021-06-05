b64c080bd2cddea5e74b0eb4e7126cc8944b53c0
Luana Bezana <luana.bezana@hotmail.com>
2021-06-05 00:30:29 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { UserLogin } from '../model/userLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('http://localhost:8080/usuario/entrar', userLogin)
+  }
+
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>('http://localhost:8080/usuario/cadastrar', user)
+
+  }
+}
